1b0d898285029342deb6f3c7f1b50a594944a3be
totoフォリオ <146707740+Yoshizawa-TPU@users.noreply.github.com>
2025-05-26 02:14:12 +0900

    AI_MINIMAXの実装

--- src/tick-tack-toe/tick-tack-toe.cpp
+++ src/tick-tack-toe/tick-tack-toe.cpp
@@ -34,10 +34,79 @@ public:
 public:
 	enum type {
 		TYPE_ORDERED = 0,
+		TYPE_MINMAX = 1,
 	};
 
 	static AI* createAi(type type);
 };
+
+//class AI_alpha_beta : public AI
+//{
+//private:
+//	int evaluate(int alpha, int beta, Board& b, Mass::status current, int& best_x, int& best_y);
+//public:
+//	AI_alpha_beta()
+//	{
+//
+//	}
+//	~AI_alpha_beta()
+//	{
+//
+//	}
+//
+//	bool think(Board& b);
+//};
+//
+//bool AI_alpha_beta::think(Board& b)
+//{
+//	int best_x, best_y;
+//	if (evaluate(-10000, 10000, b, Mass::ENEMY, best_x, best_y) <= -9999)
+//	{
+//		return false;
+//	}
+//
+//	return b.mass_[best_x][best_y].put(Mass::ENEMY);
+//}
+//
+//int AI_alpha_beta::evaluate(int alpha, int beta, Board& b, Mass::status current, int& best_x, int& best_y)
+//{
+//	Mass::status next = (current == Mass::ENEMY) ? Mass::PLAYER : Mass::ENEMY;
+//	int r = b.calc_result();
+//	if (r == current)
+//		return+10000;
+//	if (r == next)
+//		return-10000;
+//	if (r == Board::DRAW)
+//		return 0;
+//
+//	int score_max = -9999;
+//	for (int y = 0; y < Board::BOARD_SIZE; y++)
+//	{
+//		for (int x = 0;x < Board::BOARD_SIZE; x++)
+//		{
+//			Mass& m = b.mass_[y][x];
+//			if (m.getStatus() != Mass::BLANK)
+//				continue;
+//
+//			m.setStatus(current);
+//			int dummy;
+//			int score = -evaluate(-beta, -alpha, b, next, dummy, dummy);
+//			m.setStatus(Mass::BLANK);
+//			if (beta < score)
+//			{
+//				return(score_max < score) ? score : score_max;
+//			}
+//			if (score_max < score)
+//			{
+//				score_max = score;
+//				alpha = (alpha < score_max) ? score_max : alpha;
+//				best_x = x;
+//				best_y = y;
+//			}
+//		}
+//	}
+//	return score_max;
+//}
 
 // 順番に打ってみる
 class AI_ordered : public AI {
@@ -48,10 +117,24 @@ public:
 	bool think(Board& b);
 };
 
+class AI_MiniMax :public AI
+{
+private:
+	int evaluate(Board& b);
+	int minimax(Board& b, bool isMax);
+public:
+	bool think(Board& b);
+};
+
+
 AI* AI::createAi(type type)
 {
 	switch (type) {
-		// case TYPE_ORDERED:
+		 case TYPE_ORDERED:
+			 return new AI_ordered();
+		 case TYPE_MINMAX:
+			 return new AI_MiniMax();
+
 	default:
 		return new AI_ordered();
 		break;
@@ -63,6 +146,8 @@ AI* AI::createAi(type type)
 class Board
 {
 	friend class AI_ordered;
+	friend class AI_alpha_beta;
+	friend class AI_MiniMax;
 
 public:
 	enum WINNER {
@@ -194,11 +279,74 @@ bool AI_ordered::think(Board& b)
 }
 
 
+int AI_MiniMax::evaluate(Board& b)
+{
+	Board::WINNER result = b.calc_result();
+	if (result == Board::ENEMY) return 10;
+	if (result == Board::PLAYER) return -10;
+	return 0;
+}
+
+int AI_MiniMax::minimax(Board& b, bool isMax)
+{
+	Board::WINNER result = b.calc_result();
+	if (result != Board::NOT_FINISED) {
+		return evaluate(b);
+	}
+
+	int bestScore = isMax ? -1000 : 1000;
+
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			if (b.mass_[y][x].getStatus() == Mass::BLANK) {
+				b.mass_[y][x].setStatus(isMax ? Mass::ENEMY : Mass::PLAYER);
+				int score = minimax(b, !isMax);
+				b.mass_[y][x].setStatus(Mass::BLANK);
+
+				bestScore = isMax ?
+					std::max(bestScore, score) :
+					std::min(bestScore, score);
+			}
+		}
+	}
+
+	return bestScore;
+}
+
+bool AI_MiniMax::think(Board& b)
+{
+	int bestScore = -1000;
+	int bestX = -1, bestY = -1;
+
+	for (int y = 0; y < 3; y++) {
+		for (int x = 0; x < 3; x++) {
+			if (b.mass_[y][x].getStatus() == Mass::BLANK) {
+				b.mass_[y][x].setStatus(Mass::ENEMY);
+				int score = minimax(b, false);
+				b.mass_[y][x].setStatus(Mass::BLANK);
+
+				if (score > bestScore) {
+					bestScore = score;
+					bestX = x;
+					bestY = y;
+				}
+			}
+		}
+	}
+
+	if (bestX != -1 && bestY != -1) {
+		return b.mass_[bestY][bestX].put(Mass::ENEMY);
+	}
+
+	return false;
+}
+
+
 
 class Game
 {
 private:
-	const AI::type ai_type = AI::TYPE_ORDERED;
+	const AI::type ai_type = AI::TYPE_MINMAX;
 
 	Board board_;
 	Board::WINNER winner_ = Board::NOT_FINISED;
